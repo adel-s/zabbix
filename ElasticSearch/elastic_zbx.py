135e42e4e47d1ae479adccd09aec3d73281f853e
adel-s <adel.sachkov@gmail.com>
2017-05-06 21:03:43 +0300

    v.2 added: get summary info by indices groupped by name

--- ElasticSearch/elastic_zbx.py
+++ ElasticSearch/elastic_zbx.py
@@ -5,6 +5,7 @@ import sys
 import json
 import urllib
 import time
+import re
 
 ttl = 60
 
@@ -20,7 +21,7 @@ def get_cache(api):
     lock = '/tmp/elastizabbix-{0}.lock'.format(api)
     jtime = os.path.exists(cache) and os.path.getmtime(cache) or 0
     if time.time() - jtime > ttl and not os.path.exists(lock):
-        open(lock, 'a').close() 
+        open(lock, 'a').close()
         urllib.urlretrieve(stats[api], cache)
         os.remove(lock)
     ltime = os.path.exists(lock) and os.path.getmtime(lock) or None
@@ -31,13 +32,28 @@ def get_cache(api):
 def get_stat(api, stat):
     d = get_cache(api)
     keys = []
-    for i in stat.split('.'):
+    for i in stat.split(':'):
         keys.append(i)
-        key = '.'.join(keys)
-        if key in d:
-            d = d.get(key)
-            keys = []
+    for i in keys:
+        d = d[i]
     return d
+
+def get_stat_groups (api, stat):
+    d = get_cache(api)
+    keys=[]
+    for i in stat.split(':'):
+        keys.append(i)
+
+    sum=0
+    name = keys[0]
+
+    for subkey in d[api]:
+        if name in subkey:
+            dd=d[api][subkey]
+            for i in keys[1:]:
+                dd = dd[i]
+            sum += dd
+    return  sum
 
 def discover_nodes():
     d = {'data': []}
@@ -51,18 +67,21 @@ def discover_indices():
         d['data'].append({'{#NAME}': k})
     return json.dumps(d)
 
+def discover_indices_groups():
+    d = {'data': []}
+    for k,v in get_stat('indices', 'indices').iteritems():
+        match = re.match('(?:(?:^)|(?:\n))([^.]*)(-\d{4}\.\d{2}.\d{2})', k)
+        if match and not match.group(1) in (d['{#NAME}'] for d in d['data']):
+            d['data'].append({'{#NAME}': match.group(1)})
+    return json.dumps(d)
+
 if __name__ == '__main__':
     api = sys.argv[1]
     stat = sys.argv[2]
-    if api == 'discover':
-        if stat == 'nodes':
-            print discover_nodes()
-        if stat == 'indices':
-            print discover_indices()
-    else:
-        stat = get_stat(api, stat)
-        if isinstance(stat, dict):
-            print ''
-        else:
-            print stat
 
+    if api == 'discover' and stat=='nodes': print discover_nodes()
+    elif api == 'discover' and stat=='indices': print discover_indices()
+    elif api == 'discover' and stat=='indices_groups': print discover_indices_groups()
+    elif api == 'indices' or api == 'nodes' or api == 'health' or api == 'cluster': print get_stat(api, stat)
+    elif api == 'indices_groups': print get_stat_groups('indices', stat)
+    else: print '-1'
